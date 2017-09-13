f26431bd27409cfaf905c70212358de9a939dbd3
Pawel <pawel.html@gmail.com>
2017-09-13 14:04:12 +0200

    backend - running , frontend - running

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import registerServiceWorker from './registerServiceWorker';
+
+import CommentBox from './CommentBox';
+
+
+ReactDOM.render(
+    <CommentBox />, 
+    document.getElementById('root')
+);
+// registerServiceWorker();
